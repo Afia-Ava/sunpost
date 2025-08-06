34e0861cf0a70945ff917841bb028015bf69e0c9
Afia Ava <afiakhanomava@gmail.com>
2025-08-06 19:17:11 -0400

    firestore view fix

---
+++ firebase-secrets.js
@@ -0,0 +1,8 @@
+window.firebaseConfig = {
+  apiKey: 'AIzaSyAoea1mOHxpnAt9yJ9QmeVwuXARiuxZeco',
+  authDomain: 'thechatcard.firebaseapp.com',
+  projectId: 'thechatcard',
+  storageBucket: 'thechatcard.appspot.com',
+  messagingSenderId: '623571205943',
+  appId: '1:623571205943:web:fbdd5db249e594a6e0a4b2',
+};
